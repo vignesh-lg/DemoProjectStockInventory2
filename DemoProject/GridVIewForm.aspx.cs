4e03b6539f3f119765f8cd9a702336f9ebac1110
Vignesh L.G <lgvigneshkrishna@outlook.com>
2020-02-10 19:24:14 +0530

    Log_10_02_2020

--- DemoProject/GridVIewForm.aspx.cs
+++ DemoProject/GridVIewForm.aspx.cs
@@ -66,7 +66,7 @@ namespace DemoProject
                 if (userManager.GetCustomerDetails(user) == true)
                 {
                     PopulateGridView();
-                    labelsuccess.Text = "New Data Added";
+                    labelsuccess.Text = "New Data Added" ;
                     labelfailure.Text = "";
                 }
             }
